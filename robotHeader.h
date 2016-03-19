ec54c9216a804d19c965a7e8e2cb9a513de97f73
katbikker <katbikker@users.noreply.github.com>
2016-03-19 18:49:04 +0100

    Update robotHeader.h

--- robotHeader.h
+++ robotHeader.h
@@ -30,30 +30,30 @@
 #define ADC0 	    	(1 << PINA0) 	// ADC0 (Input); Free A/D channel
 // ---------------------------------------------------
 // PORTB
-#define SL4 		  (1 << PINB7) 	// Output; Left LED1 green
-#define ACS_L 		(1 << PINB6) 	// Output; Anti collision left
-#define START 		(1 << PINB5) 	// Input
-#define PWRON 		(1 << PINB4) 	// Output; Must be 1, else ACS won't work!
-#define ACS_PWRH 	(1 << PINB3) 	// Output; Regulate short/med/long ACS sensing
-#define ACS 		  (1 << PINB2) 	// INT2 (Input); ACS interrupt
-#define SL5 		  (1 << PINB1) 	// Output; Left LED2 red
-#define SL6 		  (1 << PINB0) 	// Output; Left LED3 red and left bumper
+#define SL4 		    (1 << PINB7) 	// Output; Left LED1 green
+#define ACS_L 		  (1 << PINB6) 	// Output; Anti collision left
+#define START 		  (1 << PINB5) 	// Input
+#define PWRON 		  (1 << PINB4) 	// Output; Must be 1, else ACS won't work!
+#define ACS_PWRH 	  (1 << PINB3) 	// Output; Regulate short/med/long ACS sensing
+#define ACS 		    (1 << PINB2) 	// INT2 (Input); ACS interrupt
+#define SL5 		    (1 << PINB1) 	// Output; Left LED2 red
+#define SL6 		    (1 << PINB0) 	// Output; Left LED3 red and left bumper
 // ---------------------------------------------------
 // PORTC
-#define ACS_R 		(1 << PINC7) 	// Output; Anti collision right
-#define SL3 		  (1 << PINC6) 	// Output; Right LED3 red and right bumper
-#define SL2 		  (1 << PINC5) 	// Output; Right LED2 red
-#define SL1 		  (1 << PINC4)	// Output; Right LED1 green
-#define DIR_R 		(1 << PINC3) 	// Output; Motor direction right; 0=drive;1=reverse
-#define DIR_L 		(1 << PINC2) 	// Output; Motor direction left; 0=drive;1=reverse
-#define SDA 		  (1 << PINC1)	// I2C Data (I/O)
-#define SCL 		  (1 << PINC0) 	// I2C Clock (Output (Master), Input (Slave))
+#define ACS_R 	    (1 << PINC7) 	// Output; Anti collision right
+#define SL3 		    (1 << PINC6) 	// Output; Right LED3 red and right bumper
+#define SL2 		    (1 << PINC5) 	// Output; Right LED2 red
+#define SL1 		    (1 << PINC4)	// Output; Right LED1 green
+#define DIR_R 		  (1 << PINC3) 	// Output; Motor direction right; 0=drive;1=reverse
+#define DIR_L 		  (1 << PINC2) 	// Output; Motor direction left; 0=drive;1=reverse
+#define SDA 		    (1 << PINC1)	// I2C Data (I/O)
+#define SCL 		    (1 << PINC0) 	// I2C Clock (Output (Master), Input (Slave))
 // ---------------------------------------------------
 // PORTD
-#define IRCOMM    (1 << PIND7) 	// Output; DO NOT USE THIS PIN BY YOURSELF!
-#define ACS_PWR   (1 << PIND6) 	// Output; Regulate short/med/long ACS sensing
-#define MOTOR_R   (1 << PIND5) 	// PWM Output (OC1A); Right motor on (1)/off (0)
-#define MOTOR_L   (1 << PIND4) 	// PWM Output (OC1B); Left motor on (1)/off (0)
+#define IRCOMM      (1 << PIND7) 	// Output; DO NOT USE THIS PIN BY YOURSELF!
+#define ACS_PWR     (1 << PIND6) 	// Output; Regulate short/med/long ACS sensing
+#define MOTOR_R     (1 << PIND5) 	// PWM Output (OC1A); Right motor on (1)/off (0)
+#define MOTOR_L     (1 << PIND4) 	// PWM Output (OC1B); Left motor on (1)/off (0)
 #define ENC_R     (1 << PIND3) 	// INT1 (Input); Wheel encoder_R (revolution count)
 #define ENC_L 		(1 << PIND2) 	// INT0 (Input); Wheel encoder_L (revolution count)
 #define TX 			  (1 << PIND1) 	// USART TX (Output)
