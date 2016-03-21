8464340cb18a8d696f9fe153814839ecae14bbe8
donfb <dedonfb@live.nl>
2016-03-21 11:23:00 +0100

    Update PWM.c

--- PWM.c
+++ PWM.c
@@ -3,14 +3,21 @@
 
 int main(void)
 {
+    DDRC |= (1<<6);
+    DDRB |= (1<<0);
     DDRD |= (1<<5) | (1<<4);
+    
     TCCR0 = (0<<FOC0) | (0<<WGM01) | (1<<WGM00) | (1<<COM01) | (0<<COM00) | (0<<CS02) | (0<<CS01) | (1<<CS00);
     OCR0 = 128;
-    PORTD = (1<<5) | (1<<4);
+    
+    PORTC |= (1<<6);
+    PORTB |= (1<<0);
+    
+    PORTD = (0<<5) | (0<<4);
     _delay_ms(1000);
     PORTD = (0<<5) | (0<<4);
+    
     TCCR0 = 0;
-
 
     return 0;
 }
