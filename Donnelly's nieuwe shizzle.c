1f0a74b10fe658001e5251a351d857369a653f94
donfb <dedonfb@live.nl>
2016-04-03 11:43:42 +0200

    Update Donnelly's nieuwe shizzle.c

--- Donnelly's nieuwe shizzle.c
+++ Donnelly's nieuwe shizzle.c
@@ -1,9 +1,13 @@
 #define F_CPU 16000000
 
 #include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
 
+
+volatile unsigned long milliseconden; //global variable
 volatile uint8_t RP6Opdracht;//RP6 Opdracht Register
-//bit7 noodsituatie   1 = noodsituatie
+//bit7 noodsituatie   1 = noodsituatie of niet storen
 //bit6 bumper informatie opvragen
 //bit5
 //bit4 stuurrichting. 1 = links         0 = rechts
@@ -11,6 +15,16 @@ volatile uint8_t RP6Opdracht;//RP6 Opdracht Register
 //bit2 rijrichting    1 is achteruit    0 = vooruit
 //bit1 snelheid       Bit 1 en 0 bepalen de snelheid
 //bit0 snelheid       0, 64, 128 of 192 / 256 x 100%
+
+
+unsigned long currentTime();(1<<6)
+void currentTimeInit();
+void pingClockEnable(); //enable timer
+unsigned int returnUsAndPingClockDisable();
+double ping();
+void usartInit(); //enable usart
+void trnsmitChar(unsigned char c);
+void noodsituatie();
 
 ISR(USART0_RX_vect)//usart ontvangt iets
 {
@@ -166,18 +180,44 @@ ISR(USART0_RX_vect)//usart ontvangt iets
 
 int main(void)
 {
-    void usartInit();
-    void trnsmitChar(unsigned char c);
+    unsigned long prevTime;
+    double distanceInCm;
+
     RP6Opdracht = 0;
+
     sei();
     usartInit();
+    currentTimeInit();
+
     while(1);
+    {
+        prevTime = currentTime();
+
+        while((prevTime + 500) >= currentTime());//kijk 20 keer per seconde of de robot in gevaar is
+        distanceInCm = ping();
+
+        if(distanceInCm < 7)
+        {
+            noodsituatie();
+        }
+
+        //RP6Opdracht |= (1<<6) | (1<<7);
+
+        //stuur shit naar rp6
+
+        //RP6Opdracht &= ~(1<<6) & ~(1<<7);
+
+        //als bumper geraakt is
+        //{
+            //noodsituatie();
+        //}
+    }
     return 0;
 }
 
 void usartInit()
 {
-    UBRR0L = 103; //baud rate 9600, F_CPU 16000000)
+    UBRR0L = 103; //baud rate 9600, F_CPU 16000000
 
     UCSR0B = (1<<RXEN0) | (1<<TXEN0) | (1<<RXCIE0); //enable receive data and transmit data
     UCSR0C = (1<<UCSZ00) | (1<<UCSZ01); //8 data bits and 1 stop bit
@@ -192,4 +232,106 @@ void trnsmitChar(unsigned char c)
     while((UCSR0A & (1<<UDRE0)) == 0); //Wacht tot het register leeg is
     UDR0 = c; // char weer opsturen (UDR = USART data register)
     PORTB = 0b01000000; //PortB6 aan (pin 12)
+}
+
+unsigned long currentTime()
+{
+    return milliseconden;
+}
+
+void currentTimeInit()
+{
+    TCCR0A |= (1<<WGM01); //enable ctc mode
+    TCCR0B |= (1<<CS01) | (1<<CS00); //prescaler 64.
+    OCR0A = 249;//16000000HZ dus 16000 pulsen per milliseconden. 16000 / 64 = 250. -1 want de stap van 249 naar 0 is ook een puls
+    TIMSK0 = (1<<OCIE0A);//enable compare interrupt
+    sei(); //set global interrupt flag
+}
+
+/* enable timer */
+void pingClockEnable()
+{
+    TCCR1B = (1<<CS11); //prescaler is 8
+}
+
+/* return microseconds and disable ping clock */
+unsigned int returnUsAndPingClockDisable()
+{
+    unsigned int usec;
+
+    cli(); //disable interrupts
+    usec = TCNT1; //stel usec gelijk aan aantal klokpulsen.
+    sei(); //enable interrupts
+
+    TCCR1B = 0; //disable clock
+
+    cli(); //16bit register schrijven dus liever geen interrupt
+    TCNT1 = 0; //reset clock
+    sei(); //enable interrupts
+
+    usec = usec / 2; //prescaler is 8, cpu frequency is 16mhz, 16*10^6(16mhz) / 10^6(microseconden?) / 8(prescaler) = 2 pulses per microsecond
+
+    return usec;
+}
+
+/* enable ping 28015 */
+double ping()
+{
+    unsigned int echoTime;
+    double distance;
+
+    /* send ultrasonic signal */
+    DDRL = 255; //d alles output
+    PORTL &= ~(1 << 1); //pin l1 low, clean signal
+    _delay_us(1);
+    PORTL |= (1 << 1); //pin l1 high, send signal
+    _delay_us(4);
+    PORTL &= ~(1 << 1); //pin l1 low
+
+    /*wait for input */
+    DDRL = 0; //ddrl input
+
+    while ((PINL & (1 << 1)) == 0); //VERANDERING!!!!!!!!OMG!!!!!!!!!!!!!!!WACHTEN OP SIGNAAL
+
+    /* start timer */
+    pingClockEnable(); //start 16-bit timer
+
+    /*ultrasoon signaal wordt ontvangen */
+    while ((PINL & (1 << 1)) != 0); //VERANDERING!!!!!!!!OMG!!!!!!!!!!!!!!!
+    echoTime = returnUsAndPingClockDisable();
+    distance = (double)echoTime / 29 / 2; //reken afstand uit in cm
+    return distance;
+}
+
+void noodsituatie()
+{
+    unsigned long prevTime;
+
+    RP6Opdracht = 0b10000000;
+    //stuur shit
+    trnsmitChar('n');
+    trnsmitChar('o');
+    trnsmitChar('o');
+    trnsmitChar('d');
+    trnsmitChar('s');
+    trnsmitChar('i');
+    trnsmitChar('t');
+    trnsmitChar('u');
+    trnsmitChar('a');
+    trnsmitChar('t');
+    trnsmitChar('i');
+    trnsmitChar('e');
+    trnsmitChar('\n');
+
+    prevTime = currentTime();
+    while((prevTime + 30000) >= currentTime());
+
+    RP6Opdracht |= (1<<3) | (1<<4);
+    //stuur shit
+    RP6Opdracht |= 0b10000000;
+    //wachten tot hij ver genoeg gedraait is
+
+    RP6Opdracht = 0b10000000;
+    //stuur shit
+    RP6Opdracht = 0;
 }
