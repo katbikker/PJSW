2167c89e1c2c268f459b4e652a01862003e0f057
katbikker <katbikker@users.noreply.github.com>
2016-04-05 11:36:58 +0200

    Update kompas.c

--- kompas.c
+++ kompas.c
@@ -1,6 +1,5 @@
 //KOMPAS (zie http://www.robot-electronics.co.uk/htm/cmps3tech.htm)
 
-#define MASTER_ADRES 0x08 //2560 master adres = 8
 #define KOMPAS 0x60 //1100000 = 0x60, 1100000 + R/W bit = 11000000 (0xC0 = 192, bit0 = 0 write) or 11000001 (0xC1 = 193, bit0 = 1 read)
 
 #define KOMPAS_REG2 0x02 //register 2 is 16 bits (register 2,3)
@@ -8,22 +7,28 @@
 
 void kompas(void)
 {
-    verzenden(/* KOMPAS + (bit0 = 0, write) */, KOMPAS_REG2, /* KOMPAS + (bit0 = 1, read) */;
+    verzenden(KOMPAS, KOMPAS_REG2);
     for(uint8_t i = 0; i < 8; i++) _delay_ms(250);
     
-    ontvangen(MASTER_ADRES, data, 2); //master ontvangt 2 bytes van slave (register 2,3)
+    ontvangen(KOMPAS, data, 2); //master ontvangt 2 bytes van slave (register 2,3)
     
     uint8_t highByte = data[0]; //register 2 (0-255)
     uint8_t lowByte = data[1];  //register 3
-    uint8_t bearing = ((highByte << 8) + lowByte) / 10; //0-3599 / 10 = 0-359.9
+    double bearing = ((highByte << 8) + lowByte) / 10; //0-3599 / 10 = 0-359.9
     
     writeString("\n\rBearing: "); writeInteger(bearing,10); //print bearing
 }
 
 void calKompas(void)
 {
-    verzenden(/* KOMPAS + (bit0 = 0, write) */, KOMPAS_COMREG, 0xFF);
+    verzenden(KOMPAS, KOMPAS_COMREG);
     for(uint8_t i = 0; i < 8; i++) _delay_ms(250);
+    
+    for(uint8_t i = 0; i < 3; i++)
+    {
+    verzenden(KOMPAS, 0xFF);
+    _delay_ms(10000)
+    }
 }
 
 /*
